272d6c15dffcf71ba1c18606264c3e1bd51ca07f
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-29 14:15:54 +0100

    Update project

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject planner "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.cognesence/planner "1.0.0"
+  :description "A stack-based planning algorithm in Clojure for applying STRIPS-style operators."
+  :url "https://github.com/cognesence/planner"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+               [org.clojars.cognesence/matcher "1.0.1"]])
